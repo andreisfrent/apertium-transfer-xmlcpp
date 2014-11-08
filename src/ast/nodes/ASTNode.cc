4ece70e089b874f252b55846a8b6a6da2600b3f5
Andrei Sfrent <andrei@asfrent.net>
2014-11-08 15:57:00 +0200

    ASTNode factory method.

--- src/ast/nodes/ASTNode.cc
+++ src/ast/nodes/ASTNode.cc
@@ -1,8 +1,18 @@
 #include "ASTNode.h"
 
+#include "../../parsing/XMLNode.h"
+#include "node_types.h"
+
 namespace apertium {
 namespace xml2cpp {
 ASTNode::ASTNode() {
+}
+
+ASTNode::~ASTNode() {
+}
+
+ASTNode* ASTNode::FromXMLNode(const XMLNode& xml_node) {
+  return NULL;
 }
 }; // namespace xml2cpp
 }; // namespace apertium
--- src/ast/nodes/ASTNode.h
+++ src/ast/nodes/ASTNode.h
@@ -1,11 +1,16 @@
 #ifndef __APERTIUM_XML2CPP_AST_NODE_H
 #define __APERTIUM_XML2CPP_AST_NODE_H
 
+#include "../../parsing/XMLNode.h"
+
 namespace apertium {
 namespace xml2cpp {
 class ASTNode {
  public:
   ASTNode();
+  ~ASTNode();
+
+  static ASTNode *FromXMLNode(const XMLNode& xml_node);
  private:
   ASTNode(ASTNode&);
   ASTNode(ASTNode&&);
