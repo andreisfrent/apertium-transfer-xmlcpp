a3398705c0f611da6a9839311ddc41d7eb506aa3
Andrei Sfrent <andrei@asfrent.net>
2014-11-08 02:57:05 +0200

    XML walking.

--- src/ASTBuilder.cc
+++ src/ASTBuilder.cc
@@ -1,5 +1,6 @@
 #include "ASTBuilder.h"
 
+#include <iostream>
 #include <libxml/xmlreader.h>
 #include <string>
 
@@ -31,6 +32,33 @@ ASTBuilder::~ASTBuilder() {
 }
 
 ASTNode *ASTBuilder::Build() {
+  // We assume the XML reader member is sane here.
+  int ret = xmlTextReaderRead(xmlReader_);
+  std::string indentation("");
+  while (ret == 1) {
+    int node_type = xmlTextReaderNodeType(xmlReader_);
+    xmlChar * xml_tag = xmlTextReaderName(xmlReader_);
+    if (node_type == XML_READER_TYPE_ELEMENT) {
+      std::cout << indentation << "+ " << xml_tag << std::endl;
+      indentation += "  ";
+      if (xmlTextReaderIsEmptyElement(xmlReader_)) {
+        indentation = indentation.substr(0, indentation.size() - 2);
+        std::cout << indentation << "- " << xml_tag << std::endl;
+      }
+    }
+    if (node_type == XML_READER_TYPE_END_ELEMENT) {
+      indentation = indentation.substr(0, indentation.size() - 2);
+      std::cout << indentation << "- " << xml_tag << std::endl;
+    }
+
+    // Advance to next node in XML stream.
+    ret = xmlTextReaderRead(xmlReader_);
+  }
+
+  if (ret == -1) {
+    throw std::wstring(L"Error! Could not parse input as XML.");
+  }
+
   return NULL;
 }
 } // namespace xml2cpp
