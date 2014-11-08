b81bb6f5d99adb740bd9f331ded8366ea766a113
Andrei Sfrent <andrei@asfrent.net>
2014-11-08 23:35:12 +0200

    Removed unused includes.

--- src/ast/nodes/ASTNode_Transfer.h
+++ src/ast/nodes/ASTNode_Transfer.h
@@ -1,9 +1,6 @@
 #ifndef __APERTIUM_XML2CPP_AST_NODE_TRANSFER_H
 #define __APERTIUM_XML2CPP_AST_NODE_TRANSFER_H
 
-#include <string>
-#include <unordered_map>
-
 #include "ASTNode.h"
 
 namespace apertium {
