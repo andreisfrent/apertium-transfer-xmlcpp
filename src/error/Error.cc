fae9d1c28376b27b30afe5bcf9ce31bd23c342ee
Andrei Sfrent <andrei@asfrent.net>
2014-11-10 01:02:48 +0200

    Multiple arguments as Error descriptions.

--- src/error/Error.cc
+++ src/error/Error.cc
@@ -1,17 +1,5 @@
 #include <apertium_xml2cpp.h>
-#include <cstdlib>
-#include <iostream>
-#include <string>
-
 namespace apertium {
 namespace xml2cpp {
-void Error::Fatal(const std::wstring& description) {
-   std::wcout << "\033[1;31mFATAL\033[0m " << description << std::endl;
-   exit(-1);
-}
-
-void Error::Warning(const std::wstring& description) {
-   std::wcout << "\033[1;33mWARNING\033[0m " << description << std::endl;
-}
 } // namespace xml2cpp
 } // namespace apertium
--- src/error/Error.h
+++ src/error/Error.h
@@ -1,14 +1,38 @@
 #ifndef __APERTIUM_XML2CPP_ERROR_H
 #define __APERTIUM_XML2CPP_ERROR_H
 
+#include <cstdlib>
+#include <iostream>
 #include <string>
 
 namespace apertium {
 namespace xml2cpp {
 class Error {
+ private:
+  template<typename T>
+  static void _Out(const T& t) {
+    std::wcout << t << std::endl;
+  }
+
+  template<typename T, typename ... TArgs>
+  static void _Out(const T& t, TArgs ... t_args) {
+    std::wcout << t;
+    _Out(t_args...);
+  }
+
  public:
-  static void Fatal(const std::wstring& description);
-  static void Warning(const std::wstring& description);
+  template<typename ... TArgs>
+  static void Fatal(TArgs ... t_args) {
+     std::wcout << "\033[1;31mFATAL\033[0m ";
+     _Out(t_args...);
+     exit(-1);
+  }
+
+  template<typename ... TArgs>
+  static void Warning(TArgs ... t_args) {
+     std::wcout << "\033[1;33mWARNING\033[0m ";
+     _Out(t_args...);
+  }
 };
 } // namespace xml2cpp
 } // namespace apertium
