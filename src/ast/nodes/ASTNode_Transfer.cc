f9453c717794d877d9485096d3ee1b1b075f3c9f
Andrei Sfrent <andrei@asfrent.net>
2014-11-09 02:21:21 +0200

    ASTNode_Transfer.Initialize()

--- src/ast/nodes/ASTNode_Transfer.cc
+++ src/ast/nodes/ASTNode_Transfer.cc
@@ -8,5 +8,17 @@ ASTNode_Transfer::ASTNode_Transfer() : ASTNode() {
 
 ASTNode_Transfer::~ASTNode_Transfer() {
 }
+
+void ASTNode_Transfer::Initialize(
+    const std::unordered_map<std::wstring, std::wstring>& attrs) {
+  const auto& it = attrs.find(L"default");
+  if (it == attrs.end() || it->second == L"lu") {
+    operation_mode_ = kOperationMode_LU;
+  } else if (it->second == L"chunk") {
+    operation_mode_ = kOperationMode_CHUNK;
+  } else {
+    throw std::runtime_error("Error! Bad attributes for transfer node.");
+  }
+}
 } // namespace xml2cpp
 } // namespace apertium
--- src/ast/nodes/ASTNode_Transfer.h
+++ src/ast/nodes/ASTNode_Transfer.h
@@ -9,6 +9,15 @@ class ASTNode_Transfer : public ASTNode {
  public:
   ASTNode_Transfer();
   ~ASTNode_Transfer();
+
+  const int kOperationMode_LU = 0;
+  const int kOperationMode_CHUNK = 1;
+
+  void Initialize(
+      const std::unordered_map<std::wstring, std::wstring>& attrs) override;
+
+ private:
+  int operation_mode_;
 };
 } // namespace xml2cpp
 } // namespace apertium
