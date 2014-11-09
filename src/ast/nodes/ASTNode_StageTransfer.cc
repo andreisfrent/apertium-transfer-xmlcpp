b7990648333691fa922e23e390d4f265fa4f08ef
Andrei Sfrent <andrei@asfrent.net>
2014-11-10 01:03:14 +0200

    Attribute handling on <transfer> nodes.

--- src/ast/nodes/ASTNode_StageTransfer.cc
+++ src/ast/nodes/ASTNode_StageTransfer.cc
@@ -4,9 +4,39 @@ namespace apertium {
 namespace xml2cpp {
 ASTNode_StageTransfer::ASTNode_StageTransfer(const XMLNode *xml_node)
     : ASTNode_Stage(xml_node) {
+  SetDefaultAttrs();
+  HandleXMLAttrs(xml_node);
 }
 
 ASTNode_StageTransfer::~ASTNode_StageTransfer() {
+}
+
+void ASTNode_StageTransfer::HandleXMLAttrs(const XMLNode *xml_node) {
+  for (const auto& kv : xml_node->get_attrs()) {
+    if (kv.first == L"default") {
+      HandleXMLAttr_default(kv.second);
+    } else {
+      Error::Warning("Ignoring attribute \"", kv.first, "\".");
+    }
+  }
+}
+
+void ASTNode_StageTransfer::SetDefaultAttrs() {
+  transfer_mode_ = kLu;
+}
+
+void ASTNode_StageTransfer::HandleXMLAttr_default(const std::wstring& value) {
+  if (value == L"lu") {
+    transfer_mode_ = kLu;
+  } else if (value == L"chunk") {
+    transfer_mode_ = kChunk;
+  } else {
+    Error::Fatal("Unrecognized transfer mode \"", value, "\".");
+  }
+}
+
+int ASTNode_StageTransfer::get_transfer_mode() const {
+  return transfer_mode_;
 }
 } // namespace xml2cpp
 } // namespace apertium
--- src/ast/nodes/ASTNode_StageTransfer.h
+++ src/ast/nodes/ASTNode_StageTransfer.h
@@ -9,6 +9,18 @@ class ASTNode_StageTransfer : public ASTNode_Stage {
  public:
   ASTNode_StageTransfer(const XMLNode *xml_node);
   ~ASTNode_StageTransfer();
+
+  const int kLu = 0;
+  const int kChunk = 1;
+
+  int get_transfer_mode() const;
+
+ private:
+  void HandleXMLAttrs(const XMLNode *xml_node);
+  void HandleXMLAttr_default(const std::wstring& value);
+  void SetDefaultAttrs();
+
+  int transfer_mode_;
 };
 } // namespace xml2cpp
 } // namespace apertium
