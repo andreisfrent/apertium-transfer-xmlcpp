daf88aa4c698c12e86758284b6adf63ffb70cd38
Andrei Sfrent <andrei@asfrent.net>
2014-11-09 12:44:43 +0200

    Implemented initialization for section-def-cats.

--- src/ast/nodes/ASTNode_SectionDefCats.h
+++ src/ast/nodes/ASTNode_SectionDefCats.h
@@ -1,6 +1,9 @@
 #ifndef __APERTIUM_XML2CPP_AST_NODE_SECTIONDEFCATS_H
 #define __APERTIUM_XML2CPP_AST_NODE_SECTIONDEFCATS_H
 
+#include <string>
+#include <unordered_map>
+
 #include "ASTNode.h"
 
 namespace apertium {
@@ -9,6 +12,9 @@ class ASTNode_SectionDefCats : public ASTNode {
  public:
   ASTNode_SectionDefCats();
   ~ASTNode_SectionDefCats();
+
+  void Initialize(
+      const std::unordered_map<std::wstring, std::wstring>& attrs) override;
 };
 } // namespace xml2cpp
 } // namespace apertium
