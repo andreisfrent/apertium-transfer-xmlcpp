af3437ff91d8c71ab997cfe16c36a31a5a0984dc
Andrei Sfrent <andrei@asfrent.net>
2014-11-11 02:34:00 +0200

    Implemented attribute handling for Macro AST node.

--- src/ast/nodes/ASTNode_Macro.cc
+++ src/ast/nodes/ASTNode_Macro.cc
@@ -4,9 +4,30 @@ namespace apertium {
 namespace xml2cpp {
 ASTNode_Macro::ASTNode_Macro(const XMLNode *xml_node)
     : ASTNode(xml_node) {
+  HandleXMLAttributes(xml_node);
 }
 
 ASTNode_Macro::~ASTNode_Macro() {
+}
+
+void ASTNode_Macro::HandleXMLAttributes(const XMLNode *xml_node) {
+  const auto& attrs = xml_node->get_attrs();
+
+  if (attrs.find(L"n") == attrs.end()) {
+    Error::Fatal("Macro name is missing.");
+  }
+  if (attrs.find(L"npar") == attrs.end()) {
+    Error::Fatal("Parameter count attribute is mising on <def-macro>.");
+  }
+
+  name_ = attrs.find(L"n")->second;
+  param_count_ = StringUtil::ConvertTo<int>(attrs.find(L"npar")->second);
+
+  for (const auto& kv : attrs) {
+    if (kv.first != L"n" && kv.first != L"npar") {
+      Error::Warning("Unknown attribute \"", kv.first, "\" on <def-macro>");
+    }
+  }
 }
 } // namespace xml2cpp
 } // namespace apertium
--- src/ast/nodes/ASTNode_Macro.h
+++ src/ast/nodes/ASTNode_Macro.h
@@ -9,6 +9,12 @@ class ASTNode_Macro : public ASTNode {
  public:
   ASTNode_Macro(const XMLNode *xml_node);
   ~ASTNode_Macro();
+
+  void HandleXMLAttributes(const XMLNode *xml_node);
+
+ private:
+  std::wstring name_;
+  int param_count_;
 };
 } // namespace xml2cpp
 } // namespace apertium
