0dfeea517cce9504dc80193ca6c9191bf8fd1464
Andrei Sfrent <andrei@asfrent.net>
2014-11-12 02:03:07 +0200

    Implemented ctor, dtor for CodeBlock.

--- src/ast/nodes/ASTNode_CodeBlock.cc
+++ src/ast/nodes/ASTNode_CodeBlock.cc
@@ -4,9 +4,16 @@ namespace apertium {
 namespace xml2cpp {
 ASTNode_CodeBlock::ASTNode_CodeBlock(const XMLNode *xml_node)
     : ASTNode(xml_node) {
+  for (const XMLNode *xml_child : xml_node->get_children()) {
+    ASTNode_Instruction *instruction = ASTNode_Instruction::FromXMLNode(xml_child);
+    instructions_.push_back(instruction);
+  }
 }
 
 ASTNode_CodeBlock::~ASTNode_CodeBlock() {
+  for (const ASTNode_Instruction *instruction : instructions_) {
+    delete instruction;
+  }
 }
 } // namespace xml2cpp
 } // namespace apertium
