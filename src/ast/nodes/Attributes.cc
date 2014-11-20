c97382caf9a7c3204a60f72a3d9dda035afd52ac
Andrei Sfrent <andrei@asfrent.net>
2014-11-21 01:26:01 +0200

    Refactored section-attrs with methods from XMLNode.

--- src/ast/nodes/Attributes.cc
+++ src/ast/nodes/Attributes.cc
@@ -7,12 +7,10 @@ namespace apertium {
 namespace xml2cpp {
 Attributes::Attributes(const XMLNode *xml_node)
     : ASTNode(xml_node) {
-  for (const XMLNode *xml_child : xml_node->get_children()) {
-    if (xml_child->get_tag() == L"def-attr") {
-      HandleAttrDefinition(xml_child);
-    } else {
-      Error::Fatal(*xml_node, "Unexpected <", xml_child->get_tag(), "> in attributes definition section.");
-    }
+  const std::vector<XMLNode*>& def_attr_nodes =
+    xml_node->GetChildrenByTag(L"def-attr");
+  for (const XMLNode *xml_child : def_attr_nodes) {
+    HandleAttrDefinition(xml_child);
   }
 }
 
@@ -20,32 +18,23 @@ Attributes::~Attributes() {
 }
 
 void Attributes::HandleAttrDefinition(const XMLNode *xml_node) {
-  if (xml_node->get_attrs().find(L"n") == xml_node->get_attrs().end()) {
-    Error::Fatal(*xml_node, "Attribute name is missing.");
-  }
-
-  const std::wstring& attribute = xml_node->get_attrs().find(L"n")->second;
+  xml_node->EmitWarningOnUnknownAttributes({L"n", L"c"});
+  const std::wstring& attribute = xml_node->GetMandatoryAttribute(L"n");
   if (attributes_.find(attribute) != attributes_.end()) {
-    Error::Warning(*xml_node, "Multiple definitions of attribute \"", attribute, "\".");
+    Error::Fatal(*xml_node, "Multiple definitions of attribute \"", attribute, "\".");
   }
   attributes_.insert(std::make_pair(attribute, std::set<std::wstring>()));
 
-  for (const XMLNode *xml_child : xml_node->get_children()) {
-    if (xml_child->get_tag() == L"attr-item") {
-      HandleAttrItemDefinition(xml_child, attribute);
-    } else {
-      Error::Fatal(*xml_child, "Unexpected <", xml_child->get_tag(), "> in definition of attribute \"", attribute, "\".");
-    }
+  const std::vector<XMLNode*>& attr_item_nodes =
+    xml_node->GetChildrenByTag(L"attr-item");
+  for (const XMLNode *xml_child : attr_item_nodes) {
+    HandleAttrItemDefinition(xml_child, attribute);
   }
 }
 
 void Attributes::HandleAttrItemDefinition(
     const XMLNode *xml_node, const std::wstring& attribute) {
-  if (xml_node->get_attrs().find(L"tags") == xml_node->get_attrs().end()) {
-    Error::Fatal(*xml_node, "Attribute item tags missing.");
-  }
-
-  const std::wstring& attribute_item = xml_node->get_attrs().find(L"tags")->second;
+  const std::wstring& attribute_item = xml_node->GetMandatoryAttribute(L"tags");
   attributes_[attribute].insert(attribute_item);
 }
 
