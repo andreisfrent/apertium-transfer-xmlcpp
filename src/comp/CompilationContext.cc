008abd81d6de49ebc4bf2ae3f8bea072a7ec2d86
Andrei Sfrent <andrei@asfrent.net>
2014-11-26 17:18:30 +0200

    Style fix

--- src/comp/CompilationContext.cc
+++ src/comp/CompilationContext.cc
@@ -9,15 +9,15 @@ CompilationContext::CompilationContext(AST *ast)
 void CompilationContext::AssertVariable(
     const ASTNode *calling_node, const std::wstring& name) const {
   Error::Assert(
-    ast_->get_root()->get_global_variables()->HasVariable(name),
-    *calling_node, "Variable \"", name, "\" not defined.");
+      ast_->get_root()->get_global_variables()->HasVariable(name),
+      *calling_node, "Variable \"", name, "\" not defined.");
 }
 
 void CompilationContext::AssertList(
     const ASTNode *calling_node, const std::wstring& name) const {
   Error::Assert(
-    ast_->get_root()->get_global_lists()->HasList(name),
-    *calling_node, "List \"", name, "\" not defined.");
+      ast_->get_root()->get_global_lists()->HasList(name),
+      *calling_node, "List \"", name, "\" not defined.");
 }
 } // namespace apertium
 } // namespace xml2cpp
